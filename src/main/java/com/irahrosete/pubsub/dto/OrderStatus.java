019f968ff20b474219b7efadcaa8ee471a677e8c
irah rosete <irah.rosete@outlook.com>
2022-12-20 02:30:05 +1100

    create dto

---
+++ src/main/java/com/irahrosete/pubsub/dto/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.irahrosete.pubsub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderStatus {
+    private Order order;
+    private String status; // in-progress, completed
+    private String message;
+}
